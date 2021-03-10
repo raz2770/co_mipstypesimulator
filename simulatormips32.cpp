a5c72725420baf0ad0e58a598a4187e30f244ef9
rajendra kumar <rajendrakumar2770@gmail.com>
2021-03-10 14:18:50 +0530

    added

--- simulatormips32.cpp
+++ simulatormips32.cpp
@@ -1,5 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
+class registers
+{
+    public:
+    string reg_str[32]={ "r0", "at", "v0", "v1", "a0", "a1", "a2", "a3","t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7","s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7","t8", "t9", "k0", "k1", "gp", "sp", "fp", "ra"};
+    map<string, int> reg_map;
+    int *regs = new int[32]; 
+   
+    
+};
 class screen
 {  
     public:
@@ -8,18 +17,19 @@ class screen
                 cout << "$" <<  reg_vector[i].first << "[" << setw(2) << reg_vector[i].second << "] : " << regs[reg_vector[i].second] << endl;
             }
         }
-    void print_data(map<int, int> &data, int b, int e) {
-        for (int i = b; i < e; i+=4) {
+    void print_userdata(map<int,int>&data,int b,int e)
+    {
+        for(int i=b;i<e;i+=4)
+        {
             cout << "<Memory> " << "0x" << setfill('0') << setw(8) << hex << i << " : " << dec << data[i] << endl;
         }
     }
-
-    /**
-     * Print the error if present in the program
-     */
-    void print_error(int line, string message = "Invalid instruction") {
-        cout << "Error at line " << line << " : " << message << endl;
-    } 
+    void print_data(map<int, int> &data, int bgp, int bst,int egp,int est) {
+         cout << endl << "       GLOBAL DATA       " << "          |       "<<"     STACK DATA      "<<endl;
+        for (int i = bgp, j=bst; i < egp,j<est; i+=4,j+=4) {
+            cout << "<Memory> " << "0x" << setfill('0') << setw(8) << hex << i << " : " << dec << data[i] <<"            |          "<< "<Memory> " << "0x" << setfill('0') << setw(8) << hex << j << " : " << dec << data[j]<<endl;
+        }
+    }
     
 };
 class parser
@@ -45,29 +55,28 @@ class parser
         string line;
 
         while (getline(infile, line)) 
-        { // Parse each line and store data and text labels
+        { 
             line_no++;
             code.push_back("");
             if (regex_match(line, sm, empty_e)) 
-            continue; // Ignore empty instructions
+            continue; 
             if(regex_match(line, sm, mode_e)) 
-            { // .data & .text set mode
+            { 
                 if (sm[1] == "data" && sm[2] == "") 
                 mode = 0; 
                 else if (sm[1] == "text" && sm[2] == "") 
                 mode = 1;
                 else if (sm[1] == "globl" && sm[2] == "main") 
-                { // .globl main
+                { 
 
                 }
-                else { // Error
-                    // print_error(line_no+1);
+                else { 
                     cout << "Error at line " << line_no+1 << " : " << "invalid instruction"<< endl;
                     break;
                 }
                 continue;
             }
-            if (mode == 0) { // .data
+            if (mode == 0) { 
                 if (regex_match(line, sm, data_word_e)) {
                     if (sm[2] == "word") {
                         string els = sm[3];
@@ -96,17 +105,17 @@ class parser
                     }
                     
                     else {
-                        // print_error(line_no+1);
+                        
                          cout << "Error at line " << line_no+1 << " : " << "invalid instruction"<< endl;
                         
                     }
                 }
             }
-            else if (mode == 1) { // .text
+            else if (mode == 1) { 
                 if (regex_match(line, sm, branch_e)) {
                     if(branches[sm[1]] == 0) branches[sm[1]] = line_no;
                     else {
-                        // print_error(line_no+1, "Two instructions with same label");
+                        
                          cout << "Error at line " << line_no+1 << " : " << "invalid instruction"<< endl;
                        
                     }
@@ -114,7 +123,7 @@ class parser
                 code[line_no] = line;
             }
             else {
-                // print_error(line_no+1);
+                
                  cout << "Error at line " << line_no+1 << " : " << "invalid instruction"<< endl;
                 
             }
@@ -143,35 +152,36 @@ class execution
     void execute(vector<string> & code, map<string, int> &reg_map,int *regs,map<string, int> &data_r,map<int, int> &data,map<string, int> &branches)
     {   
        
-        regex br_empty_e("\\s*(\\w+)\\s*:\\s*(#.*)?"); // Match labels with no instructuions
-        //Match  add sub mul and or nor slt
+        regex br_empty_e("\\s*(\\w+)\\s*:\\s*(#.*)?"); 
+        
         regex r_format_e("\\s*(\\w+\\s*:)?\\s*(\\w+)\\s+\\$(\\w+)\\s*,\\s*\\$(\\w+)\\s*,\\s*\\$(\\w+)\\s*(#.*)?");
-        //Match addi andi ori slti
+        
         regex i_format_e("\\s*(\\w+\\s*:)?\\s*(\\w+)\\s+\\$(\\w+)\\s*,\\s*\\$(\\w+)\\s*,\\s*(\\-?\\d+)\\s*(#.*)?");
-        //Match lw, sw
+        
         regex load_e("\\s*(\\w+\\s*:)?\\s*([a-z][a-z])\\s+\\$(\\w+)\\s*,\\s*(\\-?\\d+)\\s*\\(\\s*\\$(\\w+)\\s*\\)\\s*(#.*)?");
-        //Match la, ls, ls having label in instruction
+        
         regex la_e("\\s*(\\w+\\s*:)?\\s*la\\s+\\$(\\w+)\\s*,\\s*(\\w+)\\s*(#.*)?");
+        regex li_e("\\s*(\\w+\\s*:)?\\s*li\\s+\\$(\\w+)\\s*,\\s*(\\w+)\\s*(#.*)?");
         regex lw_e("\\s*(\\w+\\s*:)?\\s*lw\\s+\\$(\\w+)\\s*,\\s*(\\w+)\\s*(#.*)?");
         regex sw_e("\\s*(\\w+\\s*:)?\\s*sw\\s+\\$(\\w+)\\s*,\\s*(\\w+)\\s*(#.*)?");
-        //Match beq, bne
+        
         regex cond_branch_e("\\s*(\\w+\\s*:)?\\s*(\\w+)\\s+\\$(\\w+)\\s*,\\s*\\$(\\w+)\\s*,\\s*(\\w+)\\s*(#.*)?");
-        //Match j
+        
         regex jump_e("\\s*(\\w+\\s*:)?\\s*j\\s+(\\w+)\\s*(#.*)?");
-        //Match halt
+        
         regex syscall_e("\\s*(\\w+\\s*:)?\\s*syscall\\s*(#.*)?");
-        // Match labels in data
+        
        
         smatch sm;
-       for (int i = 0; i < code.size(); ++i) { // Parse instructions
-        if (code[i] == "") continue; // Empty instructions
+       for (int i = 0; i < code.size(); ++i) { 
+        if (code[i] == "") continue; 
         else if (regex_match(code[i], sm, br_empty_e)) continue;
-        else if (regex_match(code[i], sm, r_format_e)) { // R - format
+        else if (regex_match(code[i], sm, r_format_e)) { 
             int reg_d = get_reg(sm[3], reg_map);
             int reg_s = get_reg(sm[4], reg_map);
             int reg_t = get_reg(sm[5], reg_map);
             if (reg_d == -1 || reg_s == -1 || reg_t == -1) {
-                cout << "Error at line " << i+1 << " : " <<"invalid instruction"<< endl;
+                cout << "Error at line " << i+1 << " : " <<"invalid instruction R"<< endl;
                 break;
             }
             if (sm[2] == "add") 
@@ -189,64 +199,77 @@ class execution
                 regs[reg_d] = regs[reg_s] < regs[reg_t];
             }
             else {
-                cout << "Error at line " << i+1 << " : " <<"invalid instruction"<< endl;
+                cout << "Error at line " << i+1 << " : " <<"invalid instruction R ADDRESS"<< endl;
                 break;
             }
             regs[0] = 0;
         }
-        else if (regex_match(code[i], sm, i_format_e)) { // I - format
+        else if (regex_match(code[i], sm, i_format_e)) { 
             int reg_d = get_reg(sm[3], reg_map);
             int reg_s = get_reg(sm[4], reg_map);
             int imm = stoi(sm[5]);
             if (reg_d == -1 || reg_s == -1) {
-                cout << "Error at line " << i+1 << " : " <<"invalid instruction"<< endl;
+                cout << "Error at line " << i+1 << " : " <<"invalid instruction I"<< endl;
                 break;
             }
             if (sm[2] == "addi") {
+              
                 regs[reg_d] = regs[reg_s] + imm;
             }
+            
            
             else {
-                cout << "Error at line " << i+1 << " : " <<"invalid instruction"<< endl;
+                cout << "Error at line " << i+1 << " : " <<"invalid instruction I ADDRESS"<< endl;
                 break;
             }
         }
-        else if (regex_match(code[i], sm, la_e)) { // la $r, lbl
+       
+        else if (regex_match(code[i], sm, la_e)) { 
             int reg_d = get_reg(sm[2], reg_map);
             string dt = sm[3];
             int adr = data_r[dt];
             if (adr == 0 || reg_d == -1) {
-                cout << "Error at line " << i+1 << " : " <<"invalid instruction"<< endl;
+                cout << "Error at line " << i+1 << " : " <<"invalid instruction LA $R "<< endl;
                 break;
             }
             regs[reg_d] = adr;
         }
-        else if (regex_match(code[i], sm, lw_e)) { // lw $r, lbl
+        else if (regex_match(code[i], sm, lw_e)) { 
             int reg_d = get_reg(sm[2], reg_map);
             string dt = sm[3];
             int adr = data_r[dt];
             if (adr == 0 || reg_d == -1) {
-                cout << "Error at line " << i+1 << " : " <<"invalid instruction"<< endl;
+                cout << "Error at line " << i+1 << " : " <<"invalid instruction LW $R"<< endl;
                 break;
             }
             regs[reg_d] = data[adr];
         }
-        else if (regex_match(code[i], sm, sw_e)) { // sw $r, lbl
+        else if (regex_match(code[i], sm, sw_e)) { 
             int reg_d = get_reg(sm[2], reg_map);
             string dt = sm[3];
             int adr = data_r[dt];
             if (adr == 0 || reg_d == -1) {
-                 cout << "Error at line " << i+1 << " : " <<"invalid instruction"<< endl;
+                 cout << "Error at line " << i+1 << " : " <<"invalid instruction SW $r"<< endl;
                 break;
             }
             data[adr] = regs[reg_d];
         }
-        else if (regex_match(code[i], sm, load_e)) { // lw, sw
+         else if (regex_match(code[i], sm, li_e))
+        {
+            int reg_d = get_reg(sm[2], reg_map);
+            int imm = stoi(sm[3]); 
+            if ( reg_d == -1) {
+                 cout << "Error at line " << i+1 << " : " <<"invalid instruction SW $r"<< endl;
+                break;
+            } 
+            regs[reg_d]=imm;
+        }
+        else if (regex_match(code[i], sm, load_e)) {
             int reg_d = get_reg(sm[3], reg_map);
             int reg_s = get_reg(sm[5], reg_map);
             int imm = stoi(sm[4]);
             if (reg_d == -1 || reg_s == -1 || imm%4 != 0) {
-                cout << "Error at line " << i+1 << " : " <<"invalid instruction"<< endl;
+                cout << "Error at line " << i+1 << " : " <<"invalid instruction SW ,LW"<< endl;
                 break;
             }
             if (sm[2] == "lw") {
@@ -256,20 +279,20 @@ class execution
                 data[imm+regs[reg_s]] = regs[reg_d];
             }
             else {
-               cout << "Error at line " << i+1 << " : " <<"invalid instruction"<< endl;
+               cout << "Error at line " << i+1 << " : " <<"invalid instruction LW,SW ADDRESS"<< endl;
                break;
             }
         }
-        else if (regex_match(code[i], sm, cond_branch_e)) { // beq, bne
+        else if (regex_match(code[i], sm, cond_branch_e)) { 
             int reg_1 = get_reg(sm[3], reg_map);
             int reg_2 = get_reg(sm[4], reg_map);
             int br = branches[sm[5]];
             if (reg_1 == -1 || reg_2 == -1) {
-                cout << "Error at line " << i+1 << " : " <<"invalid instruction"<< endl;
+                cout << "Error at line " << i+1 << " : " <<"invalid instruction beq"<< endl;
                 break;
             }
             if (br == 0) {
-                cout << "Error at line " << i+1 << " : " <<"invalid instruction"<< endl;
+                cout << "Error at line " << i+1 << " : " <<"invalid instruction bne"<< endl;
                 break;
             }
             bool eq = (regs[reg_1] == regs[reg_2]);
@@ -293,10 +316,19 @@ class execution
             i = br-1;
         }
         else if (regex_match(code[i], sm, syscall_e)) { // halt
-            break;
+             int reg_d = get_reg("v0", reg_map);
+             if(regs[reg_d]==10)
+             break;
+             else
+             continue;
+
         }
+       
         else {
-            cout << "Error at line " << i+1 << " : " <<"invalid instruction"<< endl;
+            
+            
+            
+            cout << "Error at line " << i+1 << " : " <<"invalid instruction end"<< endl;
             break;
         }
 
@@ -306,24 +338,79 @@ class execution
     }
    
 };
-class screen
-{
 
-};
-class registers
-{
+class mipssimulator:screen ,registers,parser,execution
+{   
     public:
-    string reg_str[32]={ "r0", "at", "v0", "v1", "a0", "a1", "a2", "a3","t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7","s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7","t8", "t9", "k0", "k1", "gp", "sp", "fp", "ra"};
-    map<string, int> reg_map;
-    int *regs = new int[32]; 
+    string input_file;
    
+    mipssimulator(string input_file)
+    {
+          this->input_file=input_file;
+        
+    }
+    int run()
+    {
+    string file=input_file; 
+    int mode_exec = 0; 
+    for (int j = 0; j < 32; ++j) {
+        reg_map[reg_str[j]] = j;
+    }
+
+    vector<pair<string, int>> reg_vector; 
+    for (pair<string, int> i:reg_map) {
+        reg_vector.push_back(i);
+    }
+    
+    
+    sort(reg_vector.begin(), reg_vector.end(), [](const pair<string, int> &left, const pair<string, int> &right) {
+        return left.second < right.second;
+    });
+
+
+    cout << "MIPS Simulator" << endl << endl;
+
+    
+    vector<string> code; 
+   
+    for (int k = 0; k < 32; ++k) { 
+        regs[k] = 0;
+    }
+
+    map<int, int> data; 
+    map<string, int> data_r;
+    
+    int data_p_st = 10000000; 
+    int data_p = data_p_st;
+    int global_p = 20000000;
+    int stack_p = 90000000;
+    regs[get_reg("gp", reg_map)] = global_p;
+    regs[get_reg("sp", reg_map)] = stack_p;
+
+    
+
+    map<string, int> branches;
+    
+   
+   
+
+    parse(file, code, branches,data,data_r,data_p);
+    execute(code,reg_map,regs,data_r,data,branches);
+    
+    print_reg(regs, reg_vector); 
+    cout << endl << "USER DATA" << endl;
+    print_userdata(data, data_p_st, data_p);
+    print_data(data, global_p,stack_p-200, global_p+200,stack_p);
+   
+    return 0;
+    }
+  
+    
     
 };
-class mipssimulator
-{
-
-};
-int main()
-{
-    return 0;
+int main() {
+    string file;
+    cin>>file;
+    mipssimulator *mp=new mipssimulator(file);
+    mp->run();
 }
